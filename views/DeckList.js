8f6966d268fa2e3ce6b2b8e292c97c28a7965c58
Ørjan Ellingsen <orjellingsen@gmail.com>
2018-05-26 21:55:38 +0200

    show list of decks

--- views/DeckList.js
+++ views/DeckList.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import {
   Container,
   Button,
@@ -9,29 +9,43 @@ import {
   Body,
 } from 'native-base'
 
-import { deckStructure } from '../utils/api'
+import { deckStructure, getDecks } from '../utils/api'
+import { styles } from './styles'
 
-const DeckList = ({ navigation }) => (
-  <Container>
-    <Content>
-      <Text>Deck List</Text>
-      {Object.keys(deckStructure).map(key => (
-        <Card key={key}>
-          <CardItem>
-            <Body>
-              <Text
-                onPress={() =>
-                  navigation.navigate('IndividualDeck', { deck: key })
-                }
-              >
-                {key}
-              </Text>
-            </Body>
-          </CardItem>
-        </Card>
-      ))}
-    </Content>
-  </Container>
-)
+class DeckList extends Component {
+  state = {
+    decks: {},
+  }
+  componentDidMount() {
+    getDecks()
+      .then(JSON.parse)
+      .then(decks => {
+        this.setState(() => ({ decks }))
+      })
+  }
+  render() {
+    const { navigation } = this.props
+    const { decks } = this.state
+    return (
+      <Content style={styles.content}>
+        <Text>Deck List</Text>
+        {Object.keys(decks).map(key => (
+          <Card key={key}>
+            <CardItem
+              button={true}
+              onPress={() =>
+                navigation.navigate('IndividualDeck', { deck: key })
+              }
+            >
+              <Body>
+                <Text style={{ alignSelf: 'center' }}>{key}</Text>
+              </Body>
+            </CardItem>
+          </Card>
+        ))}
+      </Content>
+    )
+  }
+}
 
 export default DeckList
