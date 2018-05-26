e1a32076345c31a653e6666d0a82734984392cd6
Ørjan Ellingsen <orjellingsen@gmail.com>
2018-05-27 01:15:54 +0200

    add new questions

--- views/NewQuestion.js
+++ views/NewQuestion.js
@@ -1,10 +1,78 @@
-import React from 'react'
-import { Container, Button, Text } from 'native-base'
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import {
+  Container,
+  Content,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Icon,
+} from 'native-base'
+import { NavigationActions } from 'react-navigation'
+import { addCardToDeck } from '../utils/api'
+import { styles } from './styles'
 
-const NewQuestion = () => (
-  <Container>
-    <Text>New Question</Text>
-  </Container>
-)
+class NewDeck extends Component {
+  state = {
+    question: '',
+    answer: '',
+  }
 
-export default NewQuestion
+  onSubmit = () => {
+    const { title } = this.props.navigation.state.params
+    addCardToDeck(title, { ...this.state })
+    this.toHome()
+  }
+
+  onChange = (value, key) => {
+    this.setState(() => ({ [key]: value }))
+  }
+
+  toHome = () => {
+    this.props.navigation.dispatch(
+      NavigationActions.back({
+        key: 'NewQuestion',
+      })
+    )
+  }
+
+  render() {
+    const { question, answer } = this.state
+    const { title } = this.props.navigation.state.params
+    return (
+      <Content style={styles.content}>
+        <Form>
+          <Item floatingLabel>
+            <Label>Question</Label>
+            <Input
+              value={question}
+              onChangeText={value => this.onChange(value, 'question')}
+            />
+          </Item>
+          <Item floatingLabel>
+            <Label>Answer</Label>
+            <Input
+              value={answer}
+              onChangeText={value => this.onChange(value, 'answer')}
+            />
+          </Item>
+          <Button
+            style={styles.button}
+            iconLeft
+            block
+            light
+            onPress={this.onSubmit}
+          >
+            <Icon name="add" />
+            <Text>Add Question</Text>
+          </Button>
+        </Form>
+      </Content>
+    )
+  }
+}
+
+export default NewDeck
